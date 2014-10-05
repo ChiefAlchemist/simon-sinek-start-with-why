2e5cec84cdd3c81a53aace13aa14b769c7c83bc6
alchemyunited <mark.simchock@alchemyunited.com>
2014-10-05 17:29:59 -0400

    push 1

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-simon-sinek-start-with-why
-==========================
+##Simon Sinek - "Start With Why"
+####How great leaders inspire everyone to take action.
 
-Key takeaways from the book "Start With Why" by Simon Sinek. You might know Mr. Sinek from his TED talk.
+Key takeaways from the book "Start With Why" by Simon Sinek. You might know Mr. Sinek from his very popular TED talk.
+
+===================================================================================
+
+TED talk: http://www.ted.com/talks/simon_sinek_how_great_leaders_inspire_action
+
+Sinek's site: http://sinekpartners.typepad.com/refocus/
+
+Buy the book: https://www.startwithwhy.com/Books.aspx
+
+Slideshow: http://chiefalchemist.github.io/simon-sinek-start-with-why/
+
+Pinterest: http://www.pinterest.com/chiefalchemist/start-with-why-simon-sinek/
---
+++ a-edit-this/my-quotes.js
@@ -0,0 +1,69 @@
+/**
+ * Note: Please scroll to the bottom for more details. The top has been reserved for notes and such.
+ *
+ * <br> = return
+ * <br><br> = paragraph break
+ *
+ * ** If you're not familar with programming and how that relates to apostrophes, single quotes and double quotes then please use: **
+ *
+ * - apostrophe = &#39;
+ *
+ * - left single quote = &lsquo;
+ * - right single quote = &rsquo;
+ *
+ * - left double quote = &ldquo;
+ * - right double quote = &rdquo;
+ *
+ * Other special characters: http://www.degraeve.com/reference/specialcharacters.php
+ */
+ 
+var ezMQ = {
+  "ezobj":[
+  
+	// copy & paste a row at a time. 
+    {
+	  'index'		: 2,
+	  'title'		: '',
+	  'idea_size'	: 155,
+	  'idea'		: "It was such a simple, powerful and actionable idea, that I shared it with my friends. That's what we do when we find something of value, we share it with the people we love.",
+	  'quotes'		: true,
+	  'select'		: 'Value, we share it',
+	  'who'			: 'Simon Sinek',
+	  'note'		: 'Start With Why - Preface',
+	  'caption'		: setupCaption
+    },
+	// ---------
+    {
+	  'index'		: 1,
+	  'title'		: '', 
+	  'idea'		: '<img src="img/cover.jpg" alt="Cover: Start With Why - Simon Sinek">',
+	  'quotes'		: false,
+	  'select'		: 'Cover: Start With Why - Simon Sinek',
+	  'who'			: '',
+	  'note'		: '',
+	  'caption'		: setupCaption
+    }
+  ]}
+  
+
+  
+/**
+ *
+ * * * * * * * IMPORTANT  * * * * * * * 
+ * * * Order is oldest on the bottom, newest on top. 
+ * * * This is done so adding a new "page" doesn't require scrolling. Instead just stack it on top.
+ *
+ *
+ * {
+ *  'index'			: 1,   				// strictly for internal use. Makes it easier to find a page after the fact. 
+ *  'title'			: '', 				// title
+ *  'idea'			: '',  				// the main idea, aka the quote
+ *	'idea_size'		: 100,				// you can change the font-size of the idea using this. Greater than 100 is larger, smaller than 100 will make the default smaller.
+ *  'quotes'		: true, 			// use quotation marks? default value: true. '', false or no 'quote' property will all result in quotes not being applied to the 'idea'
+ *  'select'		: '',				// *Short* description to appear in the select that lists the pages
+ *  'who'			: '',				// 'who' (or what) said it. basically the first line under / after the quote
+ *  'note'			: '',				// 'note' on where, when, why, etc. they said it. and the line under 'who' is some further details is required. 
+ *  'caption'		: setupCaption		// setupCaption is the caption defined in js/setup.js, else override it here.
+ * },
+ *
+ */
